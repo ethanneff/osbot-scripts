dd567cecab1dfce6bdb8254a151b17b1281550cc
Ethan Neff <ethan.neff@eneff.com>
2019-01-15 17:28:59 -0800

    fix: remove dup npc stat

--- Attack/src/Main.java
+++ Attack/src/Main.java
@@ -94,7 +94,7 @@ public class Main extends Script {
 		Item food = inventory.getItem(n -> n != null && n.hasAction("Drink") || n.hasAction("Eat"));
 		Item bone = inventory.getItem(n -> n != null && n.hasAction("Bury"));
 		NPC npc = npcs.closest(n -> n != null && n.isAttackable() && !n.isHitBarVisible() && n.getHealthPercent() > 0
-				&& !n.isUnderAttack() && n.isAttackable() && !n.getName().contains("Rat") && map.canReach(n)
+				&& !n.isUnderAttack() && !n.getName().contains("Rat") && map.canReach(n)
 				&& n.getPosition().distance(myPlayer().getPosition()) <= distance);
 		GroundItem ground = getGroundItems().closest(
 				n -> n != null && n.getPosition().distance(myPlayer().getPosition()) <= distance && map.canReach(n)

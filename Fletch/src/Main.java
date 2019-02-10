0e205108a54f545cdf1cca9020873a4e7b6b50d1
Ethan Neff <ethan.neff@eneff.com>
2019-02-09 17:49:06 -0800

    refactor: use latest syntax for fletching

--- Fletch/src/Main.java
+++ Fletch/src/Main.java
@@ -9,41 +9,21 @@ import org.osbot.rs07.api.ui.Tab;
 import org.osbot.rs07.script.Script;
 import org.osbot.rs07.script.ScriptManifest;
 
-@ScriptManifest(author = "Me", name = "Fletch", version = -15.0, logo = "", info = "Does some noob shit")
+@ScriptManifest(author = "Me", name = "Fletch", version = 0, logo = "", info = "")
 public class Main extends Script {
 	// properties
 	private Skill skill = Skill.FLETCHING;
-	private String logType = "Maple logs";
-	private String knifeType = "Knife";
+	private String itemType = "Yew logs";
+	private String toolType = "Knife";
 	private long lastMovement;
+	private int maxIdle = 60;
 
 	@Override
 	public void onStart() throws InterruptedException {
 		lastMovement = System.nanoTime();
 		experienceTracker.start(skill);
 		tabs.open(Tab.INVENTORY);
-		interact();
-	}
-
-	private int tick() {
-		return random(100, 300);
-	}
-
-	private void interact() throws InterruptedException {
-		sleep(random(1000, 1500));
-	}
-
-	private void selectMenu(Item knife, Item log, int index) throws InterruptedException {
-		knife.interact("Use");
-		log.interact();
-		interact();
-		widgets.get(270, index).interact("Make");
-		interact();
-	}
-
-	private void kill() throws InterruptedException {
-		stop();
-		interact();
+		sleep(500);
 	}
 
 	private final String formatTime(final long ms) {
@@ -58,68 +38,64 @@ public class Main extends Script {
 	public void onPaint(Graphics2D g) {
 		g.setColor(new Color(255, 255, 255));
 		g.setFont(new Font("Open Sans", Font.PLAIN, 12));
-		g.drawString("Elapsed -> " + String.valueOf(formatTime(experienceTracker.getElapsed(skill))), 2, 30);
+		g.drawString("Elapsed -> " + String.valueOf(formatTime(experienceTracker.getElapsed(skill))), 2, 50);
 		g.drawString(String.valueOf(skill) + " -> " + String.valueOf(skills.getStatic(skill)) + " "
-				+ String.valueOf(formatTime(experienceTracker.getTimeToLevel(skill))), 2, 45);
+				+ String.valueOf(formatTime(experienceTracker.getTimeToLevel(skill))), 2, 65);
 		g.drawString("Gained -> " + String.valueOf(experienceTracker.getGainedLevels(skill)) + " "
 				+ String.valueOf(experienceTracker.getGainedXP(skill)) + " "
-				+ String.valueOf(experienceTracker.getGainedXPPerHour(skill)), 2, 60);
+				+ String.valueOf(experienceTracker.getGainedXPPerHour(skill)), 2, 80);
 		g.drawRect(mouse.getPosition().x - 3, mouse.getPosition().y - 3, 6, 6);
+	}
+
+	private void kill() throws InterruptedException {
+		stop();
+		sleep(1000);
+	}
+
+	private void selectMenu(Item tool, Item item, int index) throws InterruptedException {
+		tool.interact();
+		item.interact();
+		sleep(1000);
+		widgets.get(270, index).interact("Make");
+		sleep(1000);
 	}
 
 	@Override
 	@SuppressWarnings("unchecked")
 	public int onLoop() throws InterruptedException {
 		// properties
-		Item knife = inventory.getItem(n -> n != null && n.getName().contains(knifeType));
-		Item log = inventory.getItem(n -> n != null && n.getName().contains(logType));
-		long currentTime = System.nanoTime();
+		Item tool = inventory.getItem(n -> n != null && n.getName().contains(toolType));
+		Item item = inventory.getItem(n -> n != null && n.getName().contains(itemType));
 		Player mod = players.closest(n -> n != null && n.getName().startsWith("Mod "));
+		long currentTime = System.nanoTime();
 		long seconds = (currentTime - lastMovement) / 1000000000;
 
 		// action
-		if (myPlayer().isMoving() || myPlayer().isAnimating()) {
+		if (myPlayer().isMoving() || myPlayer().isAnimating() || combat.isFighting()) {
 			lastMovement = currentTime;
-			return tick();
-		}
-
-		// exit if mod
-		if (mod != null) {
+		} else if (seconds > maxIdle || mod != null) {
 			kill();
-		}
-
-		// exit if nothing happens
-		if (seconds > 20) {
-			kill();
-		}
-
-		// prevent multiple clicks
-		if (seconds < 1) {
-			return tick();
-		}
-
-		// make
-		if (log != null && knife != null) {
-			selectMenu(knife, log, 16);
-			if (!myPlayer().isAnimating()) {
-				selectMenu(knife, log, 15);
+		} else if (item == null) {
+			bank.open();
+			bank.depositAllExcept(n -> n != null && n.getName().contains(toolType));
+			Item itemBank = bank.getItem(itemType);
+			Item toolBank = bank.getItem(toolType);
+			if (itemBank == null || (!inventory.contains(toolType) && toolBank == null)) {
+				kill();
 			}
-			return tick();
+			if (!inventory.contains(toolType)) {
+				bank.withdraw(toolType, 1);
+			}
+			bank.withdrawAll(itemType);
+			bank.close();
+		} else if (item != null && tool != null) {
+			selectMenu(tool, item, 16);
+			if (!myPlayer().isAnimating()) {
+				selectMenu(tool, item, 15);
+			}
 		}
 
-		// bank
-		bank.open();
-		if (bank.getItem(logType).getAmount() <= 0) {
-			kill();
-		}
-		bank.depositAllExcept(n -> n != null && n.getName().contains(knifeType));
-		if (!inventory.contains(knifeType)) {
-			bank.withdraw(knifeType, 1);
-		}
-		bank.withdrawAll(logType);
-		bank.close();
-		interact();
-		return tick();
+		// next
+		return random(200, 400);
 	}
-
 }
